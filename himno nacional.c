287bc5e6c2719e49ee21d0c576318dc1175c2228
jodegracia26 <85310834+jodegracia26@users.noreply.github.com>
2021-06-08 19:32:33 -0500

    Add files via upload

---
+++ himno nacional.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main ()
+{
+printf("                 HIMNO NACIONAL DE PANAMA\n\n");
+
+	printf(   "		CORO\n"               );
+	printf(" Alcanzamos por fin la victoria,\n");
+	printf(" en el campo feliz de la union,\n");
+	printf(" con ardientes fulgores de gloria,\n");
+	printf(" se ilumina la nueva nacion.\n\n");
+
+	printf(   "		ESTROFAS\n"               );
+	printf(" Es preciso cubrir con un velo,\n");	
+	printf(" Del pasado el calvario y la cruz,\n");
+	printf(" Y que adorne el azul de tu cielo,\n");
+	printf(" De concordia la esprendida luz.,\n");
+
+	printf(" El progreso acaricia tus lares.,\n");
+	printf(" Al compas de sublime cancion,\n");
+	printf(" Ves rugir a tus pies ambos mares,\n");
+	printf(" Que dan rumbo a tu noble mision.,\n");
+
+	printf(   "		CORO\n"               );
+	printf(" En tu suelo cubierto de flores,\n");
+	printf(" A los besos del tibio terral,\n");
+	printf(" Terminaron guerreros fragores,\n");
+	printf(" Solo reina el amor fraternal,\n");
+
+	printf(" Adelante la pica y la pala,\n");
+	printf(" Al trabajo sinj mas dilacion,\n");
+	printf(" Y seremos asi prez y gala,\n");
+	printf(" De este mundo feraz de Colon.,\n");
+
+return(0);
+}
